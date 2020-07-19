eed262cfe515c50a586c177687143317b7c5b0ea
Farzad FARID <farzy@farzy.org>
2020-07-20 00:07:58 +0200

    functions can return multiple values

--- functions.go
+++ functions.go
@@ -6,11 +6,19 @@ func add(x int, y int) int {
 	return x + y
 }
 
+// Group parameters of same type
 func sub(x, y int) int {
 	return x - y
+}
+
+// Return more than one value
+func swap(x, y string) (string, string) {
+	return y, x
 }
 
 func functions() {
 	fmt.Printf("%d + %d = %d\n", 42, 13, add(42, 13))
 	fmt.Printf("%d - %d = %d\n", 42, 13, sub(42, 13))
+	a, b := swap("world", "Hello")
+	fmt.Println("Swapped vars:", a, b)
 }
