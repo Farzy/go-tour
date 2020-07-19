48b194b056c54bf2ce5a6af600c785f44566cb05
Farzad FARID <farzy@farzy.org>
2020-07-20 00:03:11 +0200

    function parameters sharing the same type

--- functions.go
+++ functions.go
@@ -6,6 +6,11 @@ func add(x int, y int) int {
 	return x + y
 }
 
+func sub(x, y int) int {
+	return x - y
+}
+
 func functions() {
 	fmt.Printf("%d + %d = %d\n", 42, 13, add(42, 13))
+	fmt.Printf("%d - %d = %d\n", 42, 13, sub(42, 13))
 }
