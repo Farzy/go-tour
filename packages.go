420e66cd840e96be6e7b1bc7809319aace5ec3ba
Farzad FARID <farzy@farzy.org>
2020-07-19 23:45:17 +0200

    Add "math" multiple times, use Printf formatting

--- packages.go
+++ packages.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"math/rand"
 	"time"
 )
@@ -9,4 +10,5 @@ import (
 func packages() {
 	rand.Seed(time.Now().Unix())
 	fmt.Println("My favorite number is", rand.Intn(10))
+	fmt.Printf("Now you have %g problems.\n", math.Sqrt(7))
 }
