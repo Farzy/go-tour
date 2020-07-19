9fd5c257c3741db41ee5c7b5eb44c32981dfa895
Farzad FARID <farzy@farzy.org>
2020-07-20 00:16:34 +0200

    Named return values, naked return

--- functions.go
+++ functions.go
@@ -16,9 +16,19 @@ func swap(x, y string) (string, string) {
 	return y, x
 }
 
+// Named return values
+func split(sum int) (x, y int) {
+	x = sum * 4 / 9
+	y = sum - x
+	// Naked return
+	return
+}
+
 func functions() {
 	fmt.Printf("%d + %d = %d\n", 42, 13, add(42, 13))
 	fmt.Printf("%d - %d = %d\n", 42, 13, sub(42, 13))
 	a, b := swap("world", "Hello")
 	fmt.Println("Swapped vars:", a, b)
+	x, y := split(42)
+	fmt.Println("Named return values, split(42) = ", x, y)
 }
