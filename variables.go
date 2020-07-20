5fee17ed1164602b5062d2cc4d9007b86def7444
Farzad FARID <farzy@farzy.org>
2020-07-20 23:37:03 +0200

    Short variable declarations

--- variables.go
+++ variables.go
@@ -5,10 +5,15 @@ import "fmt"
 var c, python, java bool
 var j, k int = 1, 2
 
+//noinspection GoBoolExpressions
 func variables() {
 	var i int
 	fmt.Println(i, j, k, c, python, java)
 
 	var c2, python2, java2 = true, false, "no!"
 	fmt.Println(c2, python2, java2)
+
+	l := 3
+	c3, python3, java3 := true, false, "no!"
+	fmt.Println(l, c3, python3, java3)
 }
