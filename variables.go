12dc8d09c2d9d3bb7b99587b23321ecf52ed256b
Farzad FARID <farzy@farzy.org>
2020-07-20 10:52:42 +0200

    Variable initializers

--- variables.go
+++ variables.go
@@ -3,8 +3,12 @@ package main
 import "fmt"
 
 var c, python, java bool
+var j, k int = 1, 2
 
 func variables() {
 	var i int
-	fmt.Println(i, c, python, java)
+	fmt.Println(i, j, k, c, python, java)
+
+	var c2, python2, java2 = true, false, "no!"
+	fmt.Println(c2, python2, java2)
 }
