15c141e786434a6b95b16439aa9e16a28373da2f
Farzad FARID <farzy@farzy.org>
2020-07-21 12:29:35 +0200

    Add new version of Sqrt with different test, count iterations

--- exercice-loops-and-functions.go
+++ exercice-loops-and-functions.go
@@ -7,20 +7,39 @@ import (
 
 const Precision = 1e-14
 
-func Sqrt(x float64) float64 {
+func Sqrt(x float64) (float64, int) {
 	z := 1.0
+	iterations := 1
 	for math.Abs((z*z-x)/x) > Precision {
 		z -= (z*z - x) / (2 * z)
+		iterations += 1
 	}
-	return z
+	return z, iterations
+}
+
+func Sqrt2(x float64) (float64, int) {
+	z, z_prev := 1.0, 0.0
+	iterations := 0
+	for (math.Abs(z-z_prev) / z) > Precision {
+		z_prev = z
+		z -= (z*z - x) / (2 * z)
+		iterations += 1
+	}
+	return z, iterations
 }
 
 func exerciceLoopsAndFunctions() {
-	fmt.Printf("Sqrt(2) = %v\n", Sqrt(2))
+	res, iter := Sqrt(2)
+	fmt.Printf("Sqrt(2) = %v (%v iterations)\n", res, iter)
+	res, iter = Sqrt(2)
+	fmt.Printf("Sqrt2(2) = %v (%v iterations)\n", res, iter)
 
 	var vals = []float64{1, 4, 3, 9, 5, 25}
 
 	for _, x := range vals {
-		fmt.Printf("Sqrt(%v) = %v / math.Sqrt(%v) = %v\n", x, Sqrt(x), x, math.Sqrt(x))
+		res1, iter1 := Sqrt(x)
+		res2, iter2 := Sqrt2(x)
+		fmt.Printf("Sqrt(%v) = %v (%v iterations) / Sqrt2(%v) = %v (%v iterations) / math.Sqrt(%v) = %v\n",
+			x, res1, iter1, x, res2, iter2, x, math.Sqrt(x))
 	}
 }
