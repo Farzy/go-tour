9f2c552f8b4bc7ca364336e9da950e94bc5e8496
Farzad FARID <farzy@farzy.org>
2020-07-23 22:51:08 +0200

    Map literals

--- maps.go
+++ maps.go
@@ -15,4 +15,25 @@ func maps() {
 	}
 	fmt.Println(m["Bell Labs"])
 	fmt.Printf("map element: %#v\n", m["Bell Labs"])
+	fmt.Printf("map: %#v\n", m)
+
+	m = map[string]vertex{
+		"Bell Labs": vertex{
+			40.68433, -74.39967,
+		},
+		"Google": vertex{
+			37.42202, -122.08408,
+		},
+	}
+	fmt.Printf("literal map: %v\n", m)
+
+	m = map[string]vertex{
+		"Bell Labs": {
+			40.68433, -74.39967,
+		},
+		"Google": {
+			37.42202, -122.08408,
+		},
+	}
+	fmt.Printf("literal map with no type name: %v\n", m)
 }
