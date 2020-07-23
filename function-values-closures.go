7ceb24c9edaa6d075c6f46dabb0d46933a34d0c5
Farzad FARID <farzy@farzy.org>
2020-07-24 00:37:05 +0200

    Add function closures

--- function-values-closures.go
+++ function-values-closures.go
@@ -9,7 +9,16 @@ func compute(fn func(float64, float64) float64) float64 {
 	return fn(3, 4)
 }
 
+func adder() func(int) int {
+	sum := 0
+	return func(x int) int {
+		sum += x
+		return sum
+	}
+}
+
 func functionValuesClosures() {
+	subtitle("Function values")
 	hypot := func(x, y float64) float64 {
 		return math.Sqrt(x*x + y*y)
 	}
@@ -17,4 +26,13 @@ func functionValuesClosures() {
 
 	fmt.Println(compute(hypot))
 	fmt.Println(compute(math.Pow))
+
+	subtitle("Function closures")
+	pos, neg := adder(), adder()
+	for i := 0; i < 10; i++ {
+		fmt.Println(
+			pos(i),
+			neg(-2*i),
+		)
+	}
 }
