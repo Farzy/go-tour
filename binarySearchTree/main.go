4c2783e8577c530348f1d2959bf183409ec9ab33
Farzad FARID <farzy@farzy.org>
2020-08-02 00:55:37 +0200

    Implement and test Same

--- binarySearchTree/main.go
+++ binarySearchTree/main.go
@@ -25,14 +25,26 @@ func Walk(t *tree.Tree, ch chan int) {
 // Same determines whether the trees
 // t1 and t2 contain the same values.
 func Same(t1, t2 *tree.Tree) bool {
-	return false
+	ch1 := make(chan int)
+	ch2 := make(chan int)
+	go Walk(t1, ch1)
+	go Walk(t2, ch2)
+
+	for i := 0; i < 10; i++ {
+		v1 := <-ch1
+		v2 := <-ch2
+		if v1 != v2 {
+			return false
+		}
+	}
+	return true
 }
 
 func ConcurrentBinaryTree() {
 	rand.Seed(time.Now().UnixNano())
 
 	ch1 := make(chan int)
-	t1 := tree.New(10)
+	t1 := tree.New(1)
 	fmt.Println("Tree 1: ", t1)
 	go Walk(t1, ch1)
 	fmt.Println("Walking tree 1:")
@@ -40,4 +52,14 @@ func ConcurrentBinaryTree() {
 		v := <-ch1
 		fmt.Println(v)
 	}
+
+	t2 := tree.New(1)
+	fmt.Println("Tree 2: ", t2)
+
+	fmt.Println("Compare t1 & t2:", Same(t1, t2))
+
+	t3 := tree.New(2)
+	fmt.Println("Tree 3: ", t3)
+
+	fmt.Println("Compare t1 & t3:", Same(t1, t3))
 }
