c70c8a8ee3dd3e070be9fbf2a4cb063f9d8cd5b5
Farzad FARID <farzy@farzy.org>
2020-08-02 23:35:37 +0200

    Add Contains() helper function

--- utils/main.go
+++ utils/main.go
@@ -17,3 +17,12 @@ func Subtitle(subtitle string) {
 	fmt.Println(subtitle)
 	fmt.Println(strings.Repeat("-", len(subtitle)))
 }
+
+func Contains(stringList []string, stringSource string) bool {
+	for _, v := range stringList {
+		if v == stringSource {
+			return true
+		}
+	}
+	return false
+}
